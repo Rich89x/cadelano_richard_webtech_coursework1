ab5d10797fdc4c71a59dad5ec34f9e53d6c171b3
Rich89x <46711509+Rich89x@users.noreply.github.com>
2019-03-05 19:00:28 +0000

    Add files via upload

---
+++ Coursework/Coursework/Pages/Cypher/try morse.js
@@ -0,0 +1,68 @@
+var morse = {
+    a: ". ___/",
+    b: "___ . . ./",
+    c: "___ . ___ ./",
+    d: "___ . ./",
+    e: "./",
+    f: ". . ___ ./",
+    g: "___ ___ ./",
+    h: ". . . ./",
+    i: ". ./",
+    j: ". ___ ___ ___ ___/",
+    k: "___ . ___/",
+    l: ". ___ . ./",
+    m: "___ ___/",
+    n: "___ ./",
+    o: "___ ___ ___/",
+    p: ". ___ ___ ./",
+    q: "___ ___ . ___/",
+    r: ". ___ ./",
+    s: ". . ./",
+    t: "___/",
+    u: ". . ___/",
+    v: ". . . ___/",
+    w: ". ___ ___/",
+    x: "___ . . ___/",
+    y: "___ . ___ ___/",
+    z: "___ ___ . ./",
+    1: ". ___ ___ ___/",
+    2: ". . ___ ___ ___/",
+    3: ". . . ___ ___/",
+    4: ". . . . ___/",
+    5: ". . . . ./",
+    6: "___ . . . ./",
+    7: "___ ___ . . ./",
+    8: "___ ___ ___ . ./",
+    9: "___ ___ ___ ___ ./",
+    0: "___ ___ ___ ___ ___/",
+    " ": "//",
+};
+
+ 
+  var inp = document.getElementById("inp");
+  var butt = document.getElementById("encode_button");
+  var butt2 = document.getElementById("decode_button");
+  var out = document.getElementById("output");
+  
+  butt.addEventListener("click", function encode_morse() {
+    var conv = inp.value;
+    conv = conv.toLowerCase();
+    conv = conv.split("");
+    for (var i = 0; i < conv.length; i++) {
+      conv[i] = morse[conv[i]];
+    }
+    conv = conv.join(" ");
+    console.log(conv);
+    out.innerHTML = conv;
+    
+  });
+
+
+
+
+         
+    
+    
+    
+    
+   
